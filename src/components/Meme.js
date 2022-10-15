9555e2e884767ea9a6f73a320dd36d24726912b5
janamanzana <96806525+jajanaaa@users.noreply.github.com>
2022-10-15 02:15:46 +0200

    Working on API calls

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,59 +1,175 @@
-import React, { useState } from "react";
+// function Meme() {
+
+//   function handleChange(event) {
+//     const [name, value] = event.target;
+//     setMeme((prevMeme) => {
+//       return {
+//         ...prevMeme,
+//         [name]: value,
+//       };
+//     });
+//   }
+
+//     //
+//   }
+
+//   return (
+//     <main className="Meme">
+//       <form onSubmit={onSubmit}>
+//         <div className="top-inputs">
+//           <input
+//             type="text"
+//             placeholder="Top text"
+//             className="form-input"
+//             onChange={handleChange}
+//             name="topText"
+//             value={meme.topText}
+//           />
+//           <input
+//             type="text"
+//             placeholder="Bottom text"
+//             className="form-input"
+//             onChange={handleChange}
+//             name="bottomText"
+//             value={meme.bottomText}
+//           />
+//         </div>
+//         <button onClick={generateImg}>Get a new meme image 🖼️</button>
+//         <button type="submit">Submit</button>
+//       </form>
+//       <div className="bottom-meme">
+//         <img src={meme.randomImage} alt="Meme" className="meme-img" />
+//         <h2 className="meme-text top">{meme.topText}</h2>
+//         <h2 className="meme-text bottom">{meme.bottomText}</h2>
+//       </div>
+//     </main>
+//   );
+// }
+
+// export default Meme;
+//
+
+/*
+
+  
+ }
+    
+    function handleChange(event) {
+        const {name, value} = event.target
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            [name]: value
+        }))
+    }
+    
+    return (
+        <main>
+            <div className="form">
+                <input 
+                    type="text"
+                    placeholder="Top text"
+                    className="form--input"
+                    name="topText"
+                    value={meme.topText}
+                    onChange={handleChange}
+                />
+                <input 
+                    type="text"
+                    placeholder="Bottom text"
+                    className="form--input"
+                    name="bottomText"
+                    value={meme.bottomText}
+                    onChange={handleChange}
+                />
+                <button 
+                    className="form--button"
+                    onClick={getMemeImage}
+                >
+                    Get a new meme image 🖼
+                </button>
+            </div>
+            <div className="meme">
+                <img src={meme.randomImage} className="meme--image" />
+                <h2 className="meme--text top">{meme.topText}</h2>
+                <h2 className="meme--text bottom">{meme.bottomText}</h2>
+            </div>
+        </main>
+    )
+} */
+
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 function Meme() {
-  const [url, setUrl] = useState("https://i.imgflip.com/30b1gx.jpg");
-  const [topText, setTopText] = useState("");
+  const [allMemes, setAllMemes] = useState([]);
+  const [meme, setMeme] = useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "https://i.imgflip.com/30b1gx.jpg",
+  });
 
-  function showImg(response) {
-    const memesArray = response.data.data.memes;
-    let randomNumber = Math.floor(Math.random() * memesArray.length);
-    let randomUrl = memesArray[randomNumber].url;
-    setUrl(randomUrl);
-  }
-
-  function generateImg() {
-    const url = "https://api.imgflip.com/get_memes";
-    axios.get(url).then(showImg);
-  }
-
-  // Api caption
-  function showText(response) {
-    console.log(response);
-  }
-
-  const captionURL = "https://api.imgflip.com/caption_image";
-  const params = {
-    // template_id: template.id,
-    username: "kittycat00",
-    password: "kittycat123",
-    text0: topText,
+  const objectToQueryParam = (obj) => {
+    const params = Object.entries(obj).map(([key, value]) => `${key}=${value}`);
+    return "?" + params.join("&");
   };
 
-  axios.post(captionURL, params).then(showText);
+  useEffect(() => {
+    axios
+      .get("https://api.imgflip.com/get_memes")
+      .then((resp) => setAllMemes(resp.data.data.memes));
+  }, []);
 
-  function displayInput(event) {
-    setTopText(event.target.value);
-    console.log(event.target.value);
+  function renderImg() {
+    const randomNumber = Math.floor(Math.random() * allMemes.length);
+    const randomUrl = allMemes[randomNumber].url;
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      randomImage: randomUrl,
+    }));
   }
 
+  async function showText() {
+    const params = {
+      template_id: "181913649",
+      text0: "lool",
+      text1: "kitty",
+      username: "kittycat00",
+      password: "kittycat123",
+    };
+    const response = await axios.post(
+      `https://api.imgflip.com/caption_image${objectToQueryParam(params)}`
+    );
+    // return response.data.data.url;
+    setMeme(response.data.data);
+    console.log(response);
+  }
+  showText();
+  // console.log(meme);
+  // console.log(showText());
+  // setMeme((prevMeme, props) => ({
+  //   ...prevMeme,
+  //   randomImage: props.newUrl,
+  // }));
+  // async componentDidMount() {
+  //     const article = { title: 'React POST Request Example' };
+  //     const response = await axios.post('https://reqres.in/api/articles', article);
+  //     this.setState({ articleId: response.data.id });
+  // }
   return (
-    <main className="Meme">
-      <div>
-        <div className="top-inputs">
-          <input
-            type="text"
-            placeholder="Top text"
-            value={topText}
-            onChange={displayInput}
-          />
-          <input type="text" placeholder="Bottom text" />
-        </div>
-        <button onClick={generateImg}>Get a new meme image 🖼️</button>
-      </div>
-      <img src={url} alt="Meme" className="meme-img" />
-    </main>
+    <div>
+      <form>
+        <input type="text" placeholder="Top text" />
+        <input type="text" placeholder="Bottom text" />
+        <button type="button" onClick={renderImg}>
+          Get img
+        </button>
+        <button type="submit">Submit</button>
+      </form>
+      <img src="https://i.imgflip.com/30b1gx.jpg" />
+    </div>
   );
 }
 
 export default Meme;
+
+// Make second API call to render captions
